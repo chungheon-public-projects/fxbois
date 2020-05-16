266f0ea9da8ea3f75819d1403e41185d85bc45eb
Tony <looty89@gmail.com>
2020-05-16 16:20:16 +0800

    before pull merge

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const axios = require('axios')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -10,7 +11,7 @@ const admin = require('firebase-admin');
 
 
 admin.initializeApp({
-    databaseURL: "http://localhost:5678"
+    //databaseURL: "http://localhost:5678"
     
 })
     
@@ -23,14 +24,24 @@ exports.test = functions.https.onCall((data, context) => {
 exports.insertloan = functions.https.onCall((data, context) => {
     const db = admin.firestore()    
     let loansRef = db.collection('loans')
-    loansRef = loansRef.doc()
-    loansRef.set({
+    let newDoc = loansRef.doc() 
+    return loansRef.add({
         tenor:data.tenor,
         amt: data.amt,
-        interest: data.interest    
-    }).then(function(){
-        console.log(loansRef);
+        interest: data.interest,
+        borrower:'',
+        lender:''
+    }).then(saved => {
+        return saved;
     }).catch(err => {
         return err
     });
  })
+
+ exports.gettoploans = functions.https.onCall((data, context) =>  {
+    const db = admin.firestore()
+    let loansRef = db.collection('loans')
+
+ })
+
+ 
